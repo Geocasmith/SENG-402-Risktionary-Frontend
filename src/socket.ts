65d8f5eababbdfcb8fc78a66c072b418666d5069
George Carr-Smith <gca73@uclive.ac.nz>
2023-05-01 17:14:32 +1200

    c

--- src/socket.ts
+++ src/socket.ts
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 
 // const socket = io("http://localhost:3001");
 // const socket = io("https://132.181.18.66:3001");
-const socket = io("https://132.181.18.66:3001", { secure: true, transports: ['websocket'] });
-
+const socket = io("wss://132.181.18.66:3001");
 
 export default socket;
