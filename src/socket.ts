502b93546f7f5301ad3b54cbc96df5cfd7512708
George Carr-Smith <gca73@uclive.ac.nz>
2023-05-01 17:24:46 +1200

    c

--- src/socket.ts
+++ src/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 // const socket = io("http://localhost:3001");
 // const socket = io("https://132.181.18.66:3001");
-const socket = io("https://132.181.18.66:3001", { secure: true, transports: ['websocket'] });
+const socket = io("https://csse-risk1.canterbury.ac.nz:3001", { secure: true, transports: ['websocket'] });
 
 
 export default socket;
