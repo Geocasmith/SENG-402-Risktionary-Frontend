a0ca3a4ffe51eaad4ef75f045df5ecbc1de4981d
George Carr-Smith <gca73@uclive.ac.nz>
2023-05-02 15:47:44 +1200

    Added images

--- src/components/game/Slides.tsx
+++ src/components/game/Slides.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectVoteKey } from "../../store";
-import exampleImage from "./bus.png";
 import socket from "./../../socket";
 import GameWordAccessor from "./../helper/GameWordAccessor";
 import TopBar from "../TopBar";
@@ -21,40 +20,41 @@ const Slides: React.FC = () => {
 
   return (
     <>
-    <TopBar />
-    <div
+      <TopBar />
+      <div
         className="flex flex-col items-center justify-center bg-gray-50"
-        style={{ height: "calc(100vh - 8em)" }} // Change this line
+        style={{ height: "calc(100vh - 8em)" }}
       >
         <div className="flex flex-col items-center justify-center space-y-4">
-      <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold my-4 text-center">{word}</h2>
-        <div className="my-4">
-          <p>{description}</p>
+          <div className="container mx-auto px-4">
+            <h2 className="text-4xl font-bold my-4 text-center">{word}</h2>
+            <div className="my-4">
+              <p>{description}</p>
+            </div>
+            <div className="my-4">
+              <img
+                src={path} 
+                alt="Example"
+                className="mx-auto w-full max-w-md"
+  style={{ objectFit: "contain", maxWidth: "350px", maxHeight: "350px" }}
+              />
+            </div>
+            {isDrawing && (
+              <div className="flex justify-between mt-8">
+                <button
+                  onClick={handleNextButtonClick}
+                  className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+                >
+                  Next
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md">
+                  Summary
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-        <div className="my-4">
-          <img
-            src={exampleImage}
-            alt="Example"
-            className="mx-auto w-full max-w-md"
-          />
-        </div>
-        {isDrawing && (
-        <div className="flex justify-between mt-8">
-          <button
-            onClick={handleNextButtonClick}
-            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
-          >
-            Next
-          </button>
-          <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md">
-            Summary
-          </button>
-        </div>
-        )}
       </div>
-    </div>
-    </div>
     </>
   );
 };
--- src/components/helper/GameWordAccessor.tsx
+++ src/components/helper/GameWordAccessor.tsx
@@ -1,4 +1,52 @@
 import gameWords from "./../../resources/gameWords.json";
+import injury from "./../../resources/images/injury.png";
+import dataLoss from "./../../resources/images/data_loss.png";
+import busFactor from "./../../resources/images/bus_factor.png";
+import assignments from "./../../resources/images/assignments.png";
+import procrastination from "./../../resources/images/procrastination.png";
+import slowInternet from "./../../resources/images/slow_internet.png";
+import argumentsImage from "./../../resources/images/arguments.png";
+import knowledgeSilo from "./../../resources/images/knowledge_silo.png";
+import sleepingIn from "./../../resources/images/sleeping_in.png";
+import lastMinuteIntegration from "./../../resources/images/last_minute_integration.png";
+import swineFlu from "./../../resources/images/swine_flu.png";
+import jobs from "./../../resources/images/jobs.png";
+import earthquake from "./../../resources/images/earthquake.png";
+import acronyms from "./../../resources/images/acronyms.png";
+
+const images: {
+  injury: string;
+  dataLoss: string;
+  busFactor: string;
+  assignments: string;
+  procrastination: string;
+  slowInternet: string;
+  argumentsImage: string;
+  knowledgeSilo: string;
+  sleepingIn: string;
+  lastMinuteIntegration: string;
+  swineFlu: string;
+  jobs: string;
+  earthquake: string;
+  acronyms: string;
+  [key: string]: string;
+} = {
+  injury,
+  dataLoss,
+  busFactor,
+  assignments,
+  procrastination,
+  slowInternet,
+  argumentsImage,
+  knowledgeSilo,
+  sleepingIn,
+  lastMinuteIntegration,
+  swineFlu,
+  jobs,
+  earthquake,
+  acronyms,
+};
+
 
 interface GameWordInfo {
   word: string;
@@ -26,9 +74,12 @@ class GameWordAccessor {
     return gameWordInfo ? gameWordInfo.description : null;
   }
 
-  static getGameWordPathByKey(key: number): string | null {
+  static getGameWordPathByKey(key: number): string | undefined {
     const gameWordInfo = this.getGameWordInfoByKey(key);
-    return gameWordInfo ? gameWordInfo.path : null;
+    if (gameWordInfo) {
+      return images[gameWordInfo.path];
+    }
+    return undefined;
   }
 }
 
